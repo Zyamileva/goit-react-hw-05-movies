deeaa6e15161fd284d7636f0bc128d151e713eb2
Zyamileva <59625678+Zyamileva@users.noreply.github.com>
2024-01-27 18:18:25 +0000

    Deploying to gh-pages from @ Zyamileva/goit-react-hw-05-movies@efa047b67d3b09b9b3a51e0d6215a1394a003261 🚀

---
+++ static/js/134.f83623c7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[134],{134:function(e,t,r){r.r(t),r.d(t,{Movies:function(){return c}});r(791);var n=r(184),c=function(e){return(0,n.jsx)("div",{children:"Movies"})}}}]);
+//# sourceMappingURL=134.f83623c7.chunk.js.map
